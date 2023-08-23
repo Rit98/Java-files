a7a529950d7b1aecbabd0326d7639ce63ffdc91f
Rit98 <105317841+Rit98@users.noreply.github.com>
2023-08-23 09:09:25 +0530

    Add files via upload

---
+++ average_of_numbers.java
@@ -0,0 +1,10 @@
+public class demo2{
+    public static void main(String[] args int a, int b, int c){
+        int sum; sum=0;
+        sum = a + b + c;
+        float average; average=0;
+        average = ((a + b + c)/3);
+        System.out.println("Sum of the numbers="+sum);
+        System.out.print("average of three numbers="+average);
+    }
+}
